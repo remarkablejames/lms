ccd86fd63888b100e19846740234dac32d4b30d6
James Niyongira <lascasasjames85@gmail.com>
2024-01-15 12:04:45 -0500

    added course edit page UI

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import {db} from '@/lib/db'
+import {auth} from "@clerk/nextjs";
+import {redirect} from "next/navigation";
+import {IconBadge} from "@/components/icon-badge";
+import {LayoutDashboard} from "lucide-react";
 
-function Page({params}: { params: { courseId: string } }) {
+async function Page({params}: { params: { courseId: string } }) {
+    const {userId} = auth()
+    if (!userId) return redirect("/")
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId
+        }
+
+    })
+
+    if (!course) return redirect("/")
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId
+    ]
+
+    const totalFields = requiredFields.length
+    const completedFields = requiredFields.filter(field => field).length
+    const completionText = `${completedFields}/${totalFields} fields completed`
     return (
-        <div>
-            Course ID is {params.courseId}
+        <div className="p-6">
+            <div className={"flex items-center justify-between"}>
+                <div className={"flex flex-col gap-y-2"}>
+                    <h1 className={"text-2xl font-medium"}>
+                        Course setup
+                    </h1>
+                    <span className={"text-sm text-slate-700"}>
+                        Complete all fields: {completionText}
+                    </span>
+
+                </div>
+
+            </div>
+            <div className={"grid grid-cols-1 md:grid-cols-2 gap-6 mt-16 "}>
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard}/>
+                        <h1 className={"text-xl font-medium"}>
+                            Customize your course
+                        </h1>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
